e02e333f19247209dcc78f1803d82c68beecb917
Chris Yates <chris@clyates.plus.com>
2023-07-04 21:29:13 +0100

    Update composer, test namespace Move IBAN data file

--- src/IbanData.php
+++ src/IbanData.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright Copyright © 2022 BeastBytes - All rights reserved
+ * @copyright Copyright © 2023 BeastBytes - All rights reserved
  * @license BSD 3-Clause
  */
 
@@ -10,20 +10,23 @@ namespace BeastBytes\IBAN\PHP;
 
 use BeastBytes\IBAN\IbanDataInterface;
 use InvalidArgumentException;
-use RuntimeException;
 
 final class IbanData implements IbanDataInterface
 {
+    const COUNTRY_NOT_FOUND_EXCEPTION_MESSAGE = 'Country "{country}" not found in list of IBANs';
+    public const INVALID_DATA_EXCEPTION_MESSAGE =
+        '`$ibans` must be an array of IBAN definitions, a path to a file that returns an array of IBAN definitions, or `null` to use local data';
+
     public function __construct(private array|string|null $ibans = null)
     {
         if ($this->ibans === null) {
-            $this->ibans = require 'data.php';
+            $this->ibans = require dirname(__DIR__) . '/data/data.php';
         } elseif (is_string($this->ibans)) {
             $this->ibans = require $this->ibans;
         }
 
         if (!is_array($this->ibans)) {
-            throw new InvalidArgumentException('`$ibans` must be an array of IBAN definitions, a path to a file that returns an array of IBAN definitions, or `null` to use local data');
+            throw new InvalidArgumentException(self::INVALID_DATA_EXCEPTION_MESSAGE);
         }
     }
 
@@ -39,25 +42,29 @@ final class IbanData implements IbanDataInterface
 
     public function getFields(string $country): array
     {
-        if ($this->hasCountry($country)) {
-            return $this->ibans[$country]['fields'];
+        if (!$this->hasCountry($country)) {
+            throw new InvalidArgumentException(strtr(
+                self::COUNTRY_NOT_FOUND_EXCEPTION_MESSAGE,
+                [
+                    '{country}' => $country
+                ]
+            ));
         }
 
-        throw new InvalidArgumentException(strtr(
-            'Country "{country}" not found in list of IBANs',
-            ['{country}' => $country]
-        ));
+        return $this->ibans[$country]['fields'];
     }
 
     public function getPattern(string $country): string
     {
-        if ($this->hasCountry($country)) {
-            return $this->ibans[$country]['pattern'];
+        if (!$this->hasCountry($country)) {
+            throw new InvalidArgumentException(strtr(
+                self::COUNTRY_NOT_FOUND_EXCEPTION_MESSAGE,
+                [
+                    '{country}' => $country
+                ]
+            ));
         }
 
-        throw new InvalidArgumentException(strtr(
-            'Country "{country}" not found in list of IBANs',
-            ['{country}' => $country]
-        ));
+        return $this->ibans[$country]['pattern'];
     }
 }
--- tests/IbanDataTest.php
+++ tests/IbanDataTest.php
@@ -1,14 +1,17 @@
 <?php
 /**
- * @copyright Copyright © 2022 BeastBytes - All rights reserved
+ * @copyright Copyright © 2023 BeastBytes - All rights reserved
  * @license BSD 3-Clause
  */
 
 declare(strict_types=1);
 
-namespace Tests;
+namespace BeastBytes\IBAN\PHP\Tests;
 
 use BeastBytes\IBAN\PHP\IbanData;
+use InvalidArgumentException;
+use PHPUnit\Framework\Attributes\BeforeClass;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 class IbanDataTest extends TestCase
@@ -17,88 +20,103 @@ class IbanDataTest extends TestCase
     private static array $ibans = [];
     private static IbanData $testClass;
 
-    /**
-     * @beforeClass
-     */
+    #[BeforeClass]
     public static function init(): void
     {
-        self::$ibans = require dirname(__DIR__) . '/src/data.php';
+        self::$ibans = require dirname(__DIR__) . '/data/data.php';
         self::$testClass = new IbanData();
-
     }
 
     public function test_getting_countries()
     {
-        $this->assertCount(count(self::$goodCountries), self::$testClass->getCountries());
+        $this->assertCount(count(self::$ibans), self::$testClass->getCountries());
     }
 
-    /**
-     * @dataProvider goodCountries
-     */
+    #[DataProvider('goodCountriesProvider')]
     public function test_has_country($country)
     {
         $this->assertTrue(self::$testClass->hasCountry($country));
     }
 
-    /**
-     * @dataProvider badCountries
-     */
+    #[DataProvider('badCountriesProvider')]
     public function test_does_not_have_country($country)
     {
         $this->assertFalse(self::$testClass->hasCountry($country));
     }
 
-    /**
-     * @dataProvider goodCountries
-     */
+    #[DataProvider('goodCountriesProvider')]
     public function test_get_pattern($country)
     {
         $this->assertSame(self::$ibans[$country]['pattern'], self::$testClass->getPattern($country));
     }
 
-    /**
-     * @dataProvider goodCountries
-     */
+    #[DataProvider('goodCountriesProvider')]
     public function test_get_fields($country)
     {
         $this->assertSame(self::$ibans[$country]['fields'], self::$testClass->getFields($country));
     }
 
-    /**
-     * @dataProvider badCountries
-     */
+    #[DataProvider('badCountriesProvider')]
     public function test_bad_countries($country)
     {
-        $this->expectException(\InvalidArgumentException::class);
-        $this->expectExceptionMessage("Country \"$country\" not found in list of IBANs");
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage(strtr(
+            IbanData::COUNTRY_NOT_FOUND_EXCEPTION_MESSAGE,
+            [
+                '{country}' => $country
+            ]
+        ));
         self::$testClass->getPattern($country);
 
-        $this->expectException(\InvalidArgumentException::class);
-        $this->expectExceptionMessage("Country \"$country\" not found in list of IBANs");
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage(strtr(
+            IbanData::COUNTRY_NOT_FOUND_EXCEPTION_MESSAGE,
+            [
+                '{country}' => $country
+            ]
+        ));
         self::$testClass->getFields($country);
     }
 
-    public function goodCountries(): array
+    #[DataProvider('badDataProvider')]
+    public function test_bad_constructor($data)
     {
-        $ibans = require dirname(__DIR__) . '/src/data.php';
-        $goodCountries = [];
-
-        foreach (array_keys($ibans) as $country) {
-            $goodCountries[] = [$country];
-        }
-
-        self::$goodCountries = $goodCountries;
-        return self::$goodCountries;
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage(IbanData::INVALID_DATA_EXCEPTION_MESSAGE);
+        new IbanData(__DIR__ . "/data/$data.php");
     }
 
-    public function badCountries(): array
+    public static function goodCountriesProvider(): \Generator
     {
-        return [
+        $ibans = require dirname(__DIR__) . '/data/data.php';
+
+        foreach (array_keys($ibans) as $country) {
+            yield $country => [$country];
+        }
+    }
+
+    public static function badCountriesProvider(): \Generator
+    {
+        foreach ([
             'non-existent code' => ['XX'],
             'alpha-3 code' => ['GBR'],
             'too short' => ['G'],
             'too long' => ['GBRT'],
             'number string' => ['12']
-        ];
+        ] as $name => $value) {
+            yield $name => $value;
+        }
+    }
+
+    public static function badDataProvider(): \Generator
+    {
+        foreach (
+            [
+                'null',
+                'string'
+            ] as $name
+        ) {
+            yield $name => [$name];
+        }
     }
 }
